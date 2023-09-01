610eeeae314ec9f1c9b0381cd03261e441caf25f
sasodoma <saso.doma@gmail.com>
2023-09-01 14:52:49 +0200

    Give players in the hub a compass

--- src/main/java/eu/sasodoma/voxelvendetta/listener/PlayerJoinListener.java
+++ src/main/java/eu/sasodoma/voxelvendetta/listener/PlayerJoinListener.java
@@ -1,14 +1,21 @@
 package eu.sasodoma.voxelvendetta.listener;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Objects;
 
 /**
  * This listener handles the player joining and teleports them to the hub spawn.
+ * It also gives every player a compass to join the lobby.
  */
 public class PlayerJoinListener implements Listener {
 
@@ -20,6 +27,27 @@ public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().teleport(Objects.requireNonNull(Bukkit.getWorld(hubWorldName)).getSpawnLocation());
+        Player player = event.getPlayer();
+        if (player.getWorld().getName().equals(hubWorldName)) {
+            giveLobbyCompass(player);
+        }
+        player.teleport(Objects.requireNonNull(Bukkit.getWorld(hubWorldName)).getSpawnLocation());
+    }
+
+    @EventHandler
+    public void onPlayerChangedWorldEvent(PlayerChangedWorldEvent event) {
+        Player player = event.getPlayer();
+        if (player.getWorld().getName().equals(hubWorldName)) {
+            giveLobbyCompass(player);
+        }
+    }
+
+    private void giveLobbyCompass(Player player) {
+        player.getInventory().clear();
+        ItemStack lobbyCompass = new ItemStack(Material.COMPASS);
+        ItemMeta lobbyCompassMeta = lobbyCompass.getItemMeta();
+        lobbyCompassMeta.displayName(Component.text("Join lobby"));
+        lobbyCompass.setItemMeta(lobbyCompassMeta);
+        player.getInventory().setItem(4, lobbyCompass);
     }
 }
